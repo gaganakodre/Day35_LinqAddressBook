2a0b0087a498f9c59542c47bb7943a338ffcda00
shreegowri <gaganandh@gmail.com>
2022-07-26 07:47:00 +0530

    [shreegowri]retriving the data using using city or state

---
+++ Day33_LinqAddressBook/Day33_LinqAddressBook/AddressBookRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day33_LinqAddressBook
+{
+    
+    public class AddressBookRepo
+    {
+        public static void RetriveBasedOnCity(List<AddressBookInformation> list)
+        {
+            Console.WriteLine("\nretriving the city and state");
+            var res1 = (from p in list where p.City.Equals("Bengaluru") && (p.State.Equals("Karnataka")) select p).ToList();
+            Program.RetriveAllData(res1);
+        }
+    }
+}
